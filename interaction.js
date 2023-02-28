292d9ebe1fa05bbad5e80bae9d7520fdab3c33a4
Darius Tanz <scratchticketseb@gmail.com>
2023-02-28 14:11:54 +0000

    Feb 28, 2023, 9:11 AM

--- interaction.js
+++ interaction.js
@@ -13,7 +13,7 @@ async function loginWithEth(){
         window.web3 = await new Web3(ethereum);
         await getID();
         if (netID != 61){
-            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
+            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ropsten test network."); 
             alert("The current Metamask/Web3 network is not Ropsten, please connect to the Ethereum Classic test network.");
             showOverlay();
             return("Failed to connect")
