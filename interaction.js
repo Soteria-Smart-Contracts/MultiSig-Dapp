96a5cbae760732fa075c1a5c7d38b53dcf46ef2d
Darius Tanz <scratchticketseb@gmail.com>
2023-02-28 14:51:17 +0000

    Feb 28, 2023, 9:51 AM

--- interaction.js
+++ interaction.js
@@ -52,7 +52,7 @@ async function CreateETCProp(){
     let Memo = document.getElementById('ETCmemo').value;
     console.log(Amount, Receiver, Memo)
 
-    gas = 
+    gas = contract.methods.Crea
 
 }
 
