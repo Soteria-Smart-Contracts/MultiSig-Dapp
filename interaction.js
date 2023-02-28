e7a275a2496cd82f181fb1cc115b45c2ca77fcf0
Darius Tanz <scratchticketseb@gmail.com>
2023-02-28 14:25:56 +0000

    Feb 28, 2023, 9:25 AM

--- interaction.js
+++ interaction.js
@@ -22,7 +22,7 @@ async function loginWithEth(){
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
-        IsSigner();
+        CanSign = await IsSigner();
         console.log('Logged In')
         LoginText.innerHTML = "Connected with:" + '<br>' + account;
         LoggedIn = true;
